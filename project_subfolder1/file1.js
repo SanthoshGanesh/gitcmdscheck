5a2ffd6feec99cd03e957b264a8d91405ad4d499
santy1497 <santhosh.ganesh14011997@gmail.com>
2024-02-01 14:21:01 +0530

    branch1 commit f

--- project_subfolder1/file1.js
+++ project_subfolder1/file1.js
@@ -11,3 +11,12 @@ g=80;
 console.log("g'value added to main",g)
 c=4;
 console.log("c ",c)
+f=100;
+console.log("f value",f);
+console.log("new function added");
+function myFunction(p1, p2) {
+    return p1 % p2;
+  }
+
+console.log(myFunction(20,2));
+console.log("branch mergerd with main");
