6361de73241eee6af92f7d0dd5c46917c3bc5d2b
santy1497 <santhosh.ganesh14011997@gmail.com>
2024-02-01 12:12:20 +0530

    feat:new branch1 commit

--- project_subfolder1/file1.js
+++ project_subfolder1/file1.js
@@ -10,4 +10,9 @@ else{
 g=80;
 console.log("g'value added to main",g)
 c=4;
-console.log("c ",c)
+console.log("c ",c);
+console.log("new function added");
+function myFunction(p1, p2) {
+    return p1 % p2;
+  }
+console.log(myFunction(20,2));
