c68487429a58aba3ebf5a709cb50094c9094f31d
santy1497 <santhosh.ganesh14011997@gmail.com>
2024-02-01 12:08:13 +0530

    feat:new ticket push to main

--- project_subfolder1/file1.js
+++ project_subfolder1/file1.js
@@ -1,3 +1,11 @@
 let a=2;
 let b=3;
 console.log("a+b",a+b);
+if(a===2){
+    console.log("a's value is smallest positive even number");
+}
+else{
+    console.log("b's value is 3");
+}
+c=4;
+console.log("c ",c)
