35a0e07c5facb6f2fc9b3cbd08aa43d16e9cc99a
santy1497 <santhosh.ganesh14011997@gmail.com>
2024-02-01 14:14:23 +0530

    feat:evenno in main

--- project_subfolder1/file1.py
+++ project_subfolder1/file1.py
@@ -1,6 +1,5 @@
 def cube_number(number):
   return number**3
-
 print(cube_number(4))
 
 def square(n):
@@ -8,3 +7,10 @@ def square(n):
 print(square(6))
 
 print("square and cubes are printed")
+def evenno(x):
+  if x%2==0:
+     print(f"{x} is  even")
+  else:
+    print(f"{x} is not even")
+
+evenno(4)
