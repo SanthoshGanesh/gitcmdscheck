bdfb0920daef2e3843ea8077ef8bf1258f344c2b
santy1497 <santhosh.ganesh14011997@gmail.com>
2024-02-01 13:44:34 +0530

    branch1 13:44 change

--- project_subfolder1/file1.js
+++ project_subfolder1/file1.js
@@ -9,8 +9,11 @@ else{
 }
 c=4;
 console.log("c ",c);
+f=100;
+console.log("f value",f);
 console.log("new function added");
 function myFunction(p1, p2) {
     return p1 % p2;
   }
+
 console.log(myFunction(20,2));
